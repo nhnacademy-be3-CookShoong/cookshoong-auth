8943e7d75564165eed3263dc3e34f76d8ee088cd
koesnam <76582376+10kseok@users.noreply.github.com>
2023-07-03 21:32:05 +0900

    configuration

---
+++ src/main/java/store/cookshoong/www/cookshoongauth/CookshoongAuthApplication.java
@@ -0,0 +1,13 @@
+package store.cookshoong.www.cookshoongauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookshoongAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookshoongAuthApplication.class, args);
+    }
+
+}
