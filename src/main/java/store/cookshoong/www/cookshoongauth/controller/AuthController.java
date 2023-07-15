3c07cb5965a9bf2720677654dc1e5a93fd71736e
koesnam <76582376+10kseok@users.noreply.github.com>
2023-07-15 17:07:04 +0900

    feat: 🎸 추가 로그인 엔드포인트 구현
    로그인 요청을 받을 수 있게 하는 엔드포인트를 만들었고 로그인 요청을 처리합니다.

---
+++ src/main/java/store/cookshoong/www/cookshoongauth/controller/AuthController.java
@@ -0,0 +1,32 @@
+package store.cookshoong.www.cookshoongauth.controller;
+
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import store.cookshoong.www.cookshoongauth.model.request.LoginRequestDto;
+import store.cookshoong.www.cookshoongauth.model.response.LoginSuccessResponseDto;
+import store.cookshoong.www.cookshoongauth.model.vo.AccountInfo;
+import store.cookshoong.www.cookshoongauth.service.AuthService;
+
+/**
+ * 인증처리에 대한 엔드포인트.
+ *
+ * @author koesnam (추만석)
+ * @since 2023.07.13
+ */
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginSuccessResponseDto> login(@RequestBody LoginRequestDto loginRequestDto) {
+        AccountInfo accountInfo = authService.executeAuthentication(loginRequestDto);
+        return ResponseEntity.ok(authService.issueTokens(accountInfo));
+    }
+}
