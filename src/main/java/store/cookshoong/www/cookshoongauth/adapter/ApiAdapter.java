18df308dfaada7225dbdb1fbacb7b0e0faa4e55f
koesnam <76582376+10kseok@users.noreply.github.com>
2023-07-20 22:47:46 +0900

    feat: 🎸 추가 회원 현재 상태 조회 기능
    새로 토큰을 발급하기 전에 현재 상태가 이전 토큰에 담긴 상태와 달라져있을 수 있기 때문에 현재 상태를 조회하는 기능을 구현하였습니다.
    Gateway에서 토큰을 검증하므로 Auth 서버도 다른 API 서버와 통신하기 위해 헤더에 토큰을 달아줬습니다.

--- src/main/java/store/cookshoong/www/cookshoongauth/adapter/ApiAdapter.java
+++ src/main/java/store/cookshoong/www/cookshoongauth/adapter/ApiAdapter.java
@@ -1,11 +1,17 @@
 package store.cookshoong.www.cookshoongauth.adapter;
 
 import java.net.URI;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+import store.cookshoong.www.cookshoongauth.jwt.JsonWebTokenProvider;
 import store.cookshoong.www.cookshoongauth.model.request.LoginRequestDto;
+import store.cookshoong.www.cookshoongauth.model.response.AccountStatusResponseDto;
 import store.cookshoong.www.cookshoongauth.model.response.AuthenticationResponseDto;
 import store.cookshoong.www.cookshoongauth.property.ApiProperties;
 
@@ -20,6 +26,7 @@ import store.cookshoong.www.cookshoongauth.property.ApiProperties;
 public class ApiAdapter {
     private final RestTemplate restTemplate;
     private final ApiProperties apiProperties;
+    private final JsonWebTokenProvider jwtProvider;
 
     /**
      * 자격증명정보를 백엔드 서버에 호출한다.
@@ -36,8 +43,43 @@ public class ApiAdapter {
             .buildAndExpand(loginRequestDto.getLoginId())
             .toUri();
 
-        return restTemplate.getForEntity(uri, AuthenticationResponseDto.class)
+        HttpEntity<Void> request = new HttpEntity<>(getAuthorizedHeader());
+
+        return restTemplate.exchange(uri, HttpMethod.GET, request, AuthenticationResponseDto.class)
             .getBody();
     }
 
+
+    /**
+     * 회원의 현재상태를 가져온다.
+     *
+     * @param accountId the account id
+     * @return the account status response dto
+     */
+    public AccountStatusResponseDto fetchAccountStatus(Long accountId) {
+        URI uri = UriComponentsBuilder.fromUriString(apiProperties.getGatewayUri())
+            .pathSegment("api")
+            .pathSegment("accounts")
+            .pathSegment("{accountId}")
+            .pathSegment("status")
+            .buildAndExpand(accountId)
+            .toUri();
+
+        HttpEntity<Void> request = new HttpEntity<>(getAuthorizedHeader());
+
+        return restTemplate.exchange(uri, HttpMethod.GET, request, AccountStatusResponseDto.class)
+            .getBody();
+    }
+
+    /**
+     * API 서버와 통신하기 위한 인증 헤더.
+     *
+     * @return the authorized header
+     */
+    public HttpHeaders getAuthorizedHeader() {
+        HttpHeaders authorizedHeader = new HttpHeaders();
+        String jti = UUID.randomUUID().toString();
+        authorizedHeader.setBearerAuth(jwtProvider.createAccessToken(jti, "AUTH-SERVER"));
+        return authorizedHeader;
+    }
 }
